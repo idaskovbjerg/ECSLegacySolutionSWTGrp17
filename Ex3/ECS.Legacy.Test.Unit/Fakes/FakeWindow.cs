cc851d610ad5f2237d678a653f29be462dde33ba
Osvald Frisk <osvaldfrisk@Osvalds-MacBook-Pro.local>
2019-02-13 12:41:21 +0100

    Added the window class

---
+++ Ex3/ECS.Legacy.Test.Unit/Fakes/FakeWindow.cs
@@ -0,0 +1,13 @@
+﻿using ECS.Legacy.Interfaces;
+
+namespace ECS.Legacy.Test.Unit.Fakes
+{
+    public class FakeWindow : IWindow
+    {
+        public bool window { get; set; } = False;
+
+        public bool isOpen() => window;
+
+        public bool isClosed() => !window;
+    }
+}
---
+++ Ex3/ECS.Legacy/Interfaces/IWindow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy.Interfaces
+{
+    public interface IWindow
+    {
+        bool isOpen();
+        bool isClosed();
+    }
+}
